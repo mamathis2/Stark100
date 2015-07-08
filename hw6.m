1b2c3b041b93d17e9a44b63e0631d3bd7fb7b0ae
Mark <markmath@umich.edu>
2015-07-07 21:27:08 -0400

    added find_time file to hw6

--- hw6.m
+++ hw6.m
@@ -3,15 +3,18 @@ data = zeros(1,60);
 timeinfo = zeros(1, 4);
 
 %Get Time Info from user
-timeinfo(1) = input('Year?');
-timeinfo(2) = input('Day?');
-timeinfo(3) = input('Hour?');
-timeinfo(4) = input('Minute?');
+timeinfo(1) = input('Year? ');
+timeinfo(2) = input('Day? ');
+timeinfo(3) = input('Hour? ');
+timeinfo(4) = input('Minute? ');
 
 %Build Data
 build_data(data, timeinfo);
+% [time_info] = find_time(data);
+
 %increment_time(timeinfo);
 
 %Sine Table
+
 
 
