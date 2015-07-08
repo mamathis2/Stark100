99e3f3ecf7f7f6dfbc57759130dbf4b766cfcc52
derekjgg <kugeored@gmail.com>
2015-07-07 22:07:23 -0400

    fixed time increment bug

--- increment_time.m
+++ increment_time.m
@@ -1,9 +1,9 @@
 function [ timeinfo ] = increment_time( timeinfo )
 %Increments time by one minute, with wrapping
 
-timeinfo(4) = mod(timeinfo(4) + 1,60);
-timeinfo(3) = mod(timeinfo(3) + (timeinfo(4) == 0), 24);
-timeinfo(2) = mod(timeinfo(2) + (timeinfo(3)== 0), 365);
-timeinfo(1) = mod(timeinfo(1)+ (timeinfo(2)== 0), 100);
+timeinfo(4) = mod(timeinfo(4) + 1,60); %minutes
+timeinfo(3) = mod(timeinfo(3) + (timeinfo(4) == 0), 24); %hours
+timeinfo(2) = mod(timeinfo(2) + (timeinfo(3)== 0 && timeinfo(4)==0), 365); %days
+timeinfo(1) = mod(timeinfo(1)+ (timeinfo(2)==0 && timeinfo(3)==0 && timeinfo(4)==0), 100); %years
 
 end
