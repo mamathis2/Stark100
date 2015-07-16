93fa5d1c7bf466ecfc166dbda6c5990fdab2d7cc
Mark <markmath@umich.edu>
2015-07-15 21:50:35 -0400

    I added comments and changed the rows to columns

--- sine_table.m
+++ sine_table.m
@@ -5,20 +5,22 @@ fc = 40;    %carrier frequency
 samplesPerCycle = 32;
 samplesPerSecond = fc*samplesPerCycle;
 
-sine_data = zeros(samplesPerSecond, 60);
+%Initialize needed vectors
+sine_data = zeros(60,samplesPerSecond);
 sine_table0 = zeros(1,samplesPerSecond);
 sine_table1 = zeros(1,samplesPerSecond);
 sine_table2 = zeros(1,samplesPerSecond);
 x = 1:samplesPerSecond;
 
-%100 = 1 second
+%Create sine tables for each possible bit transmission (0, 1, and 2) that
+%is a second long in duration
 for i = 1:samplesPerCycle*fc
-    %each sine_table has one wavelengths worth of samples
-    sine_table0(i) = sin(2*pi*i/samplesPerCycle) + 6*(i >= samplesPerSecond*0.2) * sin (2*pi*i/samplesPerCycle);
-    sine_table1(i) = sin(2*pi*i/samplesPerCycle) + 6*(i >= samplesPerSecond*0.5) * sin (2*pi*i/samplesPerCycle);
-    sine_table2(i) = sin(2*pi*i/samplesPerCycle) + 6*(i >= samplesPerSecond*0.8) * sin (2*pi*i/samplesPerCycle);
+    sine_table0(1,i) = sin(2*pi*i/samplesPerCycle) + 6*(i >= samplesPerSecond*0.2) * sin (2*pi*i/samplesPerCycle);
+    sine_table1(1,i) = sin(2*pi*i/samplesPerCycle) + 6*(i >= samplesPerSecond*0.5) * sin (2*pi*i/samplesPerCycle);
+    sine_table2(1,i) = sin(2*pi*i/samplesPerCycle) + 6*(i >= samplesPerSecond*0.8) * sin (2*pi*i/samplesPerCycle);
 end
 
+%Plot the bits 0, 1, and 2 on their carrier frequency
 figure(1)
 plot(x,sine_table0);
 figure(2)
@@ -26,14 +28,16 @@ plot(x,sine_table1);
 figure(3)
 plot(x,sine_table2);
 
+%Generate a matrix that has the correct sine wave for each bit being
+%transmitted for the inputted data vector 
 for j = 1:60
     switch data(j)
         case 0
-            sine_data(1:samplesPerSecond,j) = sine_table0;
+            sine_data(j,1:samplesPerSecond) = sine_table0;
         case 1
-            sine_data(1:samplesPerSecond,j) = sine_table1;
+            sine_data(j,1:samplesPerSecond) = sine_table1;
         case 2
-            sine_data(1:samplesPerSecond,j) = sine_table2;
+            sine_data(j,1:samplesPerSecond) = sine_table2;
     end
 end    
 end
