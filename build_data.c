d7ef33787eb94c4adef124c75b570862d68040d2
Mark <markmath@umich.edu>
2015-08-10 14:18:02 -0400

    Changed build_data into a C function

--- build_data.c
+++ build_data.c
@@ -40,59 +40,53 @@ void build_data(int *data, int *timeinfo) {
 	data[6] = (minuteOnes % 8) >= 4;
 	data[7] = (minuteOnes % 4) >= 2;
 	data[8] = (minuteOnes % 2) >= 1;
+	
+	//Determine the bits for the hour tens digit
+	data[12] = hour >= 20;
+	data[13] = (hour % 20) >= 10;
+	
+	//Determine the bits for the hour ones digit
+	hourOnes = hour % 10;
+	data[15] = hourOnes >= 8;
+	data[16] = (hourOnes % 8) >= 4;
+	data[17] = (hourOnes % 4) >= 2;
+	data[18] = (hourOnes % 2) >= 1;
+	
+	//Determine the day hundreds digit
+	data[22] = day >= 200;
+	data[23] = (day % 200) >= 100;
+	
+	//Determine the day tens digit
+	dayTens = day % 100;
+	data[25] = dayTens >= 80;
+	data[26] = (dayTens % 80) >= 40;
+	data[27] = (dayTens % 40) >= 20;
+	data[28] = (dayTens % 20) >= 10;
+	
+	//Determine the day ones digit
+	dayOnes = day % 10;
+	data[30] = dayOnes >= 8;
+	data[31] = (dayOnes % 8) >= 4;
+	data[32] = (dayOnes % 4) >= 2;
+	data[33] = (dayOnes % 2) >= 1;
+	
+	//Determine the year tens digit
+	yearTens = year % 100;
+	data[45] = yearTens >= 80;
+	data[46] = (yearTens % 80) >= 40;
+	data[47] = (yearTens % 40) >= 20;
+	data[48] = (yearTens % 20) >= 10;
+	
+	//Determine the year ones digit
+	yearOnes = yearTens % 10;
+	data[50] = yearOnes >= 8;
+	data[51] = (yearOnes % 8) >= 4;
+	data[52] = (yearOnes % 4) >= 2;
+	data[53] = (yearOnes % 2) >= 1;
+	
+	//Display data
+	/*for(i = 0; i < 60; i = i+5) {
+		printf("%d	%d	%d	%d	%d", data[i], data[i+1], data[i+2], data[i+3], data[i+4]);
+	}*/
+	
 }
-
-
-
-%Determine the bits for the hour tens digit
-data(13) = hour >= 20;
-data(14) = mod(hour,20) >= 10;
-
-%Determine the bits for the hour ones digit
-hourOnes = mod(hour,10);
-data(16) = hourOnes >= 8;
-data(17) = mod(hourOnes,8) >= 4;
-data(18) = mod(hourOnes,4) >= 2;
-data(19) = mod(hourOnes,2) >= 1;
-
-
-%Determine the day hundreds digit
-data(23) = day >= 200;
-data(24) = mod(day,200) >= 100;
-
-%Determine the day tens digit
-dayTens = mod(day,100);
-data(26) = dayTens >= 80;
-data(27) = mod(dayTens,80) >= 40;
-data(28) = mod(dayTens,40) >= 20;
-data(29) = mod(dayTens,20) >= 10;
-
-%Determine the day ones digit
-dayOnes = mod(day,10);
-data(31) = dayOnes >= 8;
-data(32) = mod(dayOnes,8) >= 4;
-data(33) = mod(dayOnes,4) >= 2;
-data(34) = mod(dayOnes,2) >= 1;
-
-
-
-%Determine the year tens digit
-yearTens = mod(year,100);
-data(46) = yearTens >= 80;
-data(47) = mod(yearTens,80) >= 40;
-data(48) = mod(yearTens,40) >= 20;
-data(49) = mod(yearTens,20) >= 10;
-
-%Determine the year ones digit
-yearOnes = mod(yearTens,10);
-data(51) = yearOnes >= 8;
-data(52) = mod(yearOnes,8) >= 4;
-data(53) = mod(yearOnes,4) >= 2;
-data(54) = mod(yearOnes,2) >= 1;
-
-
-%Display all of the bits
-%disp(data);
-
-end
-
